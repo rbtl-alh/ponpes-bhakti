50d50ac6483efb1a7dee52c18f4d605a7a27ccf3
rara <rabiatulauliah06@gmail.com>
2022-09-18 03:02:47 +0700

    nav, beranda

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Beranda</title>
-</head>
-<body>
-    <center>
-        <h1>INI HOME!!</h1>
-    </center>
-</body>
-</html>
+@extends('layouts.main')
+
+<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}" type="text/css">
+
+@section('content')
+
+<section>
+{{-- START SECTION--}}
+    <section class="banner container-fluid"> 
+        <div class="row">
+            <img class="banner-img" src="{{ asset('assets/img/banner.png') }}" alt="">
+            <div class="banner-layer"></div>
+            <div class="container">
+                <div>
+                    <div class="row title">
+                        <div class="col-5">
+                            <h2 class="jdl">Pondok Pesantren Bhakti Bapak Emak</h2>
+                        </div>
+                    </div>
+                    <div class="row ket">
+                        <div class="col-7">
+                            <p class="ket-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias unde ullam iure quam officiis voluptas natus enim magnam possimus a, exercitationem in nesciunt dignissimos ipsa ad nostrum, cupiditate corrupti, animi quae nam dolores. Veritatis, dolore.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>   
+    </section>
+    
+    <section class="program container mt-4">
+        <div class="row d-flex justify-content-center">
+            <h2 class="text-center" style="font-weight: 700; color: #1C6758;">
+                Program
+            </h2>
+        </div>
+        <div class="row d-flex justify-content-around my-4">
+            <div class="col-3">
+                <div class="card p-card" style="width: 18rem;">
+                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-center">Program 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card p-card" style="width: 18rem;">
+                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-center">Program 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card p-card" style="width: 18rem;">
+                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-center">Program 1</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div>
+            </div>
+        </div>    
+    </section>
+
+    <section class="visimisi container mt-4">
+        <div class="row d-flex justify-content-center">
+            <h2 class="text-center" style="font-weight: 700; color: #1C6758;">
+                Visi & Misi
+            </h2>
+        </div>
+    </section>
+
+
+{{-- END SECTION--}}
+</section>
+
+
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <img class="mx-4" src="{{ asset('assets/img/logo1.png') }}" alt="" width="150">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse mx-4" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown" style="color: black">
+                <a class="nav-link nav-black dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  Profil
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Struktur Organisasi</a>
+                  <a class="dropdown-item" href="#">Sejarah</a>                  
+                  <a class="dropdown-item" href="#">Visi Misi</a>
+                  <a class="dropdown-item" href="#">Daftar Ustadz</a>                  
+                  <a class="dropdown-item" href="#">Unit</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-black dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  Pendidikan
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Sistem Pengajar dan Pendidikan</a>
+                  <a class="dropdown-item" href="#">Kurikulum</a>        
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-black dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  Santri
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Data Santri</a>
+                  <a class="dropdown-item" href="#">Aktivitas Santri</a>        
+                </div>
+              </li>
+          <li class="nav-item">
+            <a class="nav-link nav-black" href="#">Berita</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link nav-black" href="#">Galeri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-pen px-4" role="button" href="#" style="color: white">Pendaftaran</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>    
