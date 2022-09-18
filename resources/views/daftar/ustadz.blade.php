cdf58722d4b9086d00da7a7f8a710884d5c3a4c7
sulthan08 <sulthankhalish78@gmail.com>
2022-09-19 02:35:55 +0700

    SEMANGATKK

---
+++ resources/views/daftar/ustadz.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.main')
+
+<link rel="stylesheet" href="{{ asset('assets/css/daftar.css') }}" type="text/css">
+@section('content')
+<section class="banner container-fluid"> 
+    <div class="row">
+        <img class="banner-img" src="{{ asset('assets/img/fotoustadz.png') }}" alt="">
+        <div class="banner-layer"></div>
+        <div class="container">
+            <div>
+                <div class="row title d-flex justify-content-center">
+                    <div class="col-7">
+                        <h2 class="jdl text-center" >Daftar Ustadz & Ustadzah</h2>
+                        <h2 class="jdl text-center" ></h2>
+                        <h2 class="jdl text-center" ></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row mt-4 mb-4">
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <div class="row">
+
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card" style="width: 13rem;">
+                    <img src="{{ asset('assets/img/siluet.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">BAMBANG</h3>
+                      <p class="card-text text-center">guru ngaji</p>
+                    </div>
+                </div>
+            </div>
+            
+          
+        </div>
+        
+
+
+    </div>
+</section>   
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/berita', function () {
     return view('berita.index');
+});
+
+Route::get('/daftarustadz', function () {
+    return view('daftar.ustadz');
 });
