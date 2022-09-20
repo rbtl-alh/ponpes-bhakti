aa0764f133aa0d99a21e64d634db591946afbc97
rara <rabiatulauliah06@gmail.com>
2022-09-20 15:28:48 +0700

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,12 +13,12 @@
             <div class="container">
                 <div>
                     <div class="row title">
-                        <div class="col-5">
+                        <div class="col-md-5">
                             <h2 class="jdl">Pondok Pesantren Bhakti Bapak Emak</h2>
                         </div>
                     </div>
                     <div class="row ket">
-                        <div class="col-7">
+                        <div class="col-md-7">
                             <p class="ket-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias unde ullam iure quam officiis voluptas natus enim magnam possimus a, exercitationem in nesciunt dignissimos ipsa ad nostrum, cupiditate corrupti, animi quae nam dolores. Veritatis, dolore.</p>
                         </div>
                     </div>
@@ -27,41 +27,49 @@
         </div>   
     </section>
     
-    <section class="program container mt-4">
-        <div class="row d-flex justify-content-center">
-            <h2 class="text-center" style="font-weight: 700; color: #1C6758;">
-                Program
-            </h2>
+    <section class="program">
+        <div class="container mt-4">
+            <div class="row d-flex justify-content-center">
+                <h2 class="text-center" style="font-weight: 700; color: #1C6758;">
+                    Program
+                </h2>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-1">
+                    <hr class="p-hr">
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-3">
+                    <div class="card p-card mt-4">
+                        <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-center">Program 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card p-card mt-4">
+                        <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-center">Program 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card p-card mt-4">
+                        <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-center">Program 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row d-flex justify-content-around my-4">
-            <div class="col-3">
-                <div class="card p-card" style="width: 18rem;">
-                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title text-center">Program 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card p-card" style="width: 18rem;">
-                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title text-center">Program 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="card p-card" style="width: 18rem;">
-                    <img src="{{ asset('assets/img/icon-quran.png') }}" class="card-img-top mx-auto mt-3" alt="...">
-                    <div class="card-body text-center">
-                        <h5 class="card-title text-center">Program 1</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
-            </div>
-        </div>    
+
     </section>
 
     <section class="visimisi container mt-4">
@@ -69,6 +77,174 @@
             <h2 class="text-center" style="font-weight: 700; color: #1C6758;">
                 Visi & Misi
             </h2>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-1">
+                <hr class="p-hr">
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center pt-4">
+            <div class="card vm-card">
+                <div class="card-body">
+                    <div class="row justify-content-end">
+                        <div class="col-4">
+                            <div class="card vm-atr">
+                                <div class="card-body"></div>
+                                <div class="card">
+                                    <div class="card-body"></div>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+                  <div class="row">
+                    <div class="col-md-4">
+                        <div class="card pht-card">
+                            <img class="card-img-top vm-img px-4" src="{{ asset('assets/img/person.png') }}" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-md-8 pb-3 vm">
+                        <h4>Visi</h4>
+                        <hr class="vm-hr">
+                        <p>Menjadi Kawah condro dimuko generasi muslim yang manusiawi, cerdas dalam hidup, serta professional sebagai makhluk.</p>
+                        <h4 class="mt-4">Misi</h4>
+                        <hr class="vm-hr">
+                        <div class="row">
+                            <div class="col-2">
+                                <p>1.</p>
+                            </div>
+                            <div class="col" style="margin-left: -3rem;">
+                                <p>Membina budaya kesalihan ( kesalihan individu dan kesalihan social ) di kalangan santri dan masyarakat.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <p>2.</p>
+                            </div>
+                            <div class="col" style="margin-left: -3rem;">
+                                <p>Mengembangkan dan melestarikan ilmu-ilmu agama Islam yang tertuang dalam kitab-kitab kuning dan litelatur-litelatur modern.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <p>3.</p>
+                            </div>
+                            <div class="col" style="margin-left: -3rem;">
+                                <p>Menyelenggarakan kegiatan KBM sesuai dengan SOP yang telah disepakati bersama.</p>
+                            </div>
+                        </div>
+                    </div>                    
+                  </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="fakta container-fulid mt-4">
+        <div class="container">
+            <div class="row justify-content-center pt-4">
+                <div class="col-md-5">
+                    <h2 class="text-center"  style="font-weight: 700; color: #EEF2E6;">
+                        Fakta Pondok Pesantren Bhakti Bapak Emak
+                    </h2>                
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-1">
+                    <hr class="p-hr" style="background-color: #EEF2E6 !important; border: 2px solid #EEF2E6;">
+                </div>
+            </div>            
+            <div class="row r-circle d-flex justify-content-center">
+                <div class="col-md-4">
+                    <div class="card" style="background-color: transparent !important; border: none;">
+                        <div class="d-flex justify-content-center">
+                            <div class="card-text circle">                        
+                                <h4 class="text text-center">2022</h4>  
+                            </div>
+                        </div>
+                        <p class="text-center mt-3 f-ket" style="font-size: 1.3rem;color: #EEF2E6;">Tahun Berdiri</p>                        
+                    </div>
+                </div>
+                {{-- // --}}
+                <div class="col-md-4">
+                    <div class="card" style="background-color: transparent !important; border: none;">
+                        <div class="d-flex justify-content-center">
+                            <div class="card-text circle">                        
+                                <h4 class="text text-center">39</h4>                        
+                            </div>
+                        </div>
+                        <p class="text-center mt-3 f-ket" style="font-size: 1.3rem;color: #EEF2E6;">Ustadz</p>
+                    </div>
+                </div>
+                {{-- // --}}
+                <div class="col-md-4">
+                    <div class="card" style="background-color: transparent !important; border: none;">
+                        <div class="d-flex justify-content-center">
+                            <div class="card-text circle">                        
+                                <h4 class="text text-center">17</h4>
+                            </div>
+                        </div>
+                        <p class="text-center mt-3 f-ket" style="font-size: 1.3rem;color: #EEF2E6;">Ustadzah</p>
+                    </div>
+                </div>
+                {{-- // --}}
+                <div class="col-md-4">
+                    <div class="card" style="background-color: transparent !important; border: none;">
+                        <div class="d-flex justify-content-center">
+                            <div class="card-text circle">                        
+                                <h4 class="text text-center">54</h4>                        
+                            </div>
+                        </div>
+                        <p class="text-center mt-3 f-ket" style="font-size: 1.3rem;color: #EEF2E6;">Siswa</p>
+                    </div>
+                </div>
+                {{-- // --}}
+                <div class="col-md-4">
+                    <div class="card" style="background-color: transparent !important; border: none;">
+                        <div class="d-flex justify-content-center">
+                            <div class="card-text circle">                        
+                                <h4 class="text text-center">34</h4>                        
+                            </div>
+                        </div>
+                        <p class="text-center mt-3 f-ket" style="font-size: 1.3rem;color: #EEF2E6;">Siswi</p>
+                    </div>
+                </div>
+                {{-- // --}}
+            </div>
+        </div>
+    </section>
+
+    <section class="berita container mt-4">
+        <div class="row justify-content-around">
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
         </div>
     </section>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,113 @@
-
-<section class="container-fluid footer">
-    <div class="row">
-        SEMANGATTTTT
+<section class="container-fluid footer py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row ">
+                    <div class="col-5">
+                        <img class="ml-4" src="{{ asset('assets/img/logo-bwh.png') }}" alt="" width="175">
+                    </div>
+                    <div class="col-4">
+                        <div class="row d-flex">
+                            <div class="col-3">
+                                <img src="{{ asset('assets/img/icons/ig.png') }}" alt="" width="40">
+                            </div>
+                            <div class="col-sm-4">
+                                <p class="pt-2">@popesbhaktibapakemak</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="{{ asset('assets/img/icons/fb.png') }}" alt="" width="40">
+                            </div>
+                            <div class="col-sm-4">
+                                <p class="pt-2">@popesbhaktibapakemak</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                <img class="p-1" src="{{ asset('assets/img/icons/gmail.png') }}" alt="" width="40">
+                            </div>
+                            <div class="col-sm-4">
+                                <p class="pt-2">@popesbhaktibapakemak</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="{{ asset('assets/img/icons/wa.png') }}" alt="" width="40">
+                            </div>
+                            <div class="col-sm-4">
+                                <p class="pt-2">@popesbhaktibapakemak</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mx-2 mt-4 moto">
+                    <div class="row mb-1">
+                        <div class="col pl-3 mt-2">
+                            <h5>Berakhlak, Agamis, Cerdas dan Terampil</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-10 pl-3">
+                            <hr class="f-line"> 
+                        </div>
+                    </div>
+                </div>
+            </div>            
+            <div class="col-md-6 pl-4 maps">                
+                {{-- <div id="map" style="width:100%;height:400px;"></div> --}}
+                <div class="mapouter ml-4">
+                    <div class="gmap_canvas">
+                        <iframe width="542" 
+                            height="301" 
+                            id="gmap_canvas" 
+                            src="https://maps.google.com/maps?q=ponpes%20bhakti%20bapak%20emak&t=&z=15&ie=UTF8&iwloc=&output=embed" 
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
+                        {{-- <a href="https://123movies-to.org">123movies</a><br> --}}
+                        <style>
+                            .mapouter{
+                                position:relative;
+                                text-align:right;
+                                height:265px;
+                                width:472px;
+                            }
+                        </style>
+                        {{-- <a href="https://www.embedgooglemap.net"></a> --}}
+                        <style>
+                            .gmap_canvas {
+                                overflow:hidden;
+                                background:none!important;
+                                height:265px;
+                                width:472px;
+                                border: 1px solid #D6CDA4;
+                                border-radius: 12px;
+                                margin-top: 7px;
+                                }
+                        </style>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </section>
+
+<script>
+    function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+    }
+
+    window.initMap = initMap;
+</script>
