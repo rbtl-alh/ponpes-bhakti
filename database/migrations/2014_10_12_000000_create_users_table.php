1d64dbb095970739a3016e1e1ec731b587b9c4ec
rara <rabiatulauliah06@gmail.com>
2022-09-27 23:50:14 +0700

    galeri & user db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('role')->default('member');
             $table->rememberToken();
             $table->timestamps();
         });
