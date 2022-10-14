6cac915919146c7ed10fcbda2b7c993a4806defd
rara <rabiatulauliah06@gmail.com>
2022-10-14 19:01:42 +0700

    siswa

---
+++ database/migrations/2022_10_11_045601_create_siswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['Perempuan', 'Laki-laki']);
+            $table->char('nisn', 30);
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
